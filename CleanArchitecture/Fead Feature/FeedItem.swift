f000f11822790b39add66b38340f3e2519e33874
faiyazla <fyizahmed@gmail.com>
2025-08-12 10:28:31 +0545

    Fixed compile error

---
+++ CleanArchitecture/Fead Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 12/08/2025.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
