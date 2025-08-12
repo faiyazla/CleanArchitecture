f1335616eb5b940e50711bb8bcc24b1fa640e15d
faiyazla <fyizahmed@gmail.com>
2025-08-12 10:11:20 +0545

    Create the FeedItem model

---
+++ CleanArchitecture/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 12/08/2025.
+//
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
