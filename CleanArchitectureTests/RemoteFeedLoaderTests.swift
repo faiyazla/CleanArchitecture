d932e06a15c2873e2be4aef52d51f83424c74533
faiyazla <fyizahmed@gmail.com>
2025-08-18 09:50:21 +0545

    Delivers invalidData error on 200 HTTP response with invalid json

--- CleanArchitectureTests/RemoteFeedLoaderTests.swift
+++ CleanArchitectureTests/RemoteFeedLoaderTests.swift
@@ -59,6 +59,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
 
     }
+    
+    func test_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data(bytes: "invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(capturedErrors, [.invalidData])
+        
+    }
 
     
     //MARK- Helpers
@@ -84,12 +96,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completions(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil)!
-            messages[index].completions(.success(response))
+            messages[index].completions(.success(data, response))
         }
     }
 }
