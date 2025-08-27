f11ce8400ddffd4aea3134f6c957cccaeb6ef00b
faiyazla <fyizahmed@gmail.com>
2025-08-27 12:59:41 +0545

    Invert if logic to make code paths easier to follow

--- CleanArchitectureTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ CleanArchitectureTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -27,13 +27,13 @@ class LocalFeedLoader {
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
-            if error == nil {
+            if let error = error {
+                completion(error)
+            } else {
                 store.insert(items, currentDate: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
