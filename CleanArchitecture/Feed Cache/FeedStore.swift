9aebdf44e73c10abebec23a181bcbe71665ad377
faiyazla <fyizahmed@gmail.com>
2025-08-27 13:07:42 +0545

    Move `FeedStore` to its own file

---
+++ CleanArchitecture/Feed Cache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 27/08/2025.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], currentDate: Date, completion: @escaping InsertionCompletion)
+}
