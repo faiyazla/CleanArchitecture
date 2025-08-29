e2a50aaa50c90ed31621b68f0fb64ba81442fd80
faiyazla <fyizahmed@gmail.com>
2025-08-29 10:07:18 +0545

    Extract local member into properties

--- CleanArchitecture/Feed Cache/LocalFeedLoader.swift
+++ CleanArchitecture/Feed Cache/LocalFeedLoader.swift
@@ -14,6 +14,12 @@ public final class LocalFeedLoader {
     
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
+    
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -52,8 +58,7 @@ public final class LocalFeedLoader {
     }
     
     private func validate(_ timestamp: Date) -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
         return currentDate() < maxCacheAge
     }
 }
