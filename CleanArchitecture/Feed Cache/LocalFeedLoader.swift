3057c559b270fb44a660557eee0a6c289a426274
faiyazla <fyizahmed@gmail.com>
2025-09-02 12:26:08 +0545

    Rename FeedStore insert parameter for currentDate to timestamp to improve readability

--- CleanArchitecture/Feed Cache/LocalFeedLoader.swift
+++ CleanArchitecture/Feed Cache/LocalFeedLoader.swift
@@ -34,7 +34,7 @@ extension LocalFeedLoader {
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
-        store.insert(feed.toLocal(), currentDate: self.currentDate()) { [weak self] error in
+        store.insert(feed.toLocal(), timestamp: self.currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
         }
--- CleanArchitectureTests/Feed Cache/Helper/FeedStoreSpy.swift
+++ CleanArchitectureTests/Feed Cache/Helper/FeedStoreSpy.swift
@@ -38,9 +38,9 @@ class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
     
-    func insert(_ feed: [LocalFeedImage], currentDate: Date, completion: @escaping InsertionCompletion) {
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
-        receivedMessages.append(.insert(feed, currentDate))
+        receivedMessages.append(.insert(feed, timestamp))
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
