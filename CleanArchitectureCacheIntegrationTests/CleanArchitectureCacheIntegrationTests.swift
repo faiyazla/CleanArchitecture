15414e8a284ea77f2c508591d66f14fbc21d8129
faiyazla <fyizahmed@gmail.com>
2025-09-03 15:55:41 +0545

    Add CleanArchitectureCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ CleanArchitectureCacheIntegrationTests/CleanArchitectureCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  CleanArchitectureCacheIntegrationTests.swift
+//  CleanArchitectureCacheIntegrationTests
+//
+//  Created by Faiyaz Ahmed on 03/09/2025.
+//
+
+import XCTest
+
+final class CleanArchitectureCacheIntegrationTests: XCTestCase {
+
+
+}
