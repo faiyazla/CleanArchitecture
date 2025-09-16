cf96ea84c7f4633e700df1be139b5b18b85ac5b2
faiyazla <fyizahmed@gmail.com>
2025-09-16 21:34:30 +0545

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ CleanArchitectureiOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,29 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 16/09/2025.
+//
+
+import CleanArchitecture
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            switch result {
+            case .success(let feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case .failure(let error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
