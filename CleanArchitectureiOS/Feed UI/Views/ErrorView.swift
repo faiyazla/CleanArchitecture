dc5b235e31c1767509efa1f3e4f133259d4e81fd
faiyazla <fyizahmed@gmail.com>
2025-09-22 14:47:08 +0545

    Move `ErrorView` to separate file

---
+++ CleanArchitectureiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 22/09/2025.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
