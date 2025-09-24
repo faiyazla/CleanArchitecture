cca33475b1fea274abcddd7929ae733bfc093db7
faiyazla <fyizahmed@gmail.com>
2025-09-24 07:50:57 +0545

    Rename test case class to reflect the use case it refers to

--- CleanArchitectureTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ CleanArchitectureTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedImageDataLoaderTests.swift
+//  LoadFeedImageDataFromRemoteUseCaseTests.swift
 //  CleanArchitecture
 //
 //  Created by Faiyaz Ahmed on 23/09/2025.
@@ -8,7 +8,7 @@
 import XCTest
 import CleanArchitecture
 
-final class RemoteFeedImageDataLoaderTests: XCTestCase {
+final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
