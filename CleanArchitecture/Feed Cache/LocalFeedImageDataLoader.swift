29ae23f46ac1727077e92de6e65a2bd15269a895
faiyazla <fyizahmed@gmail.com>
2025-09-24 12:12:31 +0545

    Move LocalFeedImageDataLoader and FeedImageDataStore to production

---
+++ CleanArchitecture/Feed Cache/LocalFeedImageDataLoader.swift
@@ -0,0 +1,59 @@
+//
+//  LocalFeedImageDataLoader.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 24/09/2025.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
+
+public final class LocalFeedImageDataLoader {
+    
+    private let store: FeedImageDataStore
+    
+    public init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    public enum Error: Swift.Error {
+        case failed
+        case notFound
+    }
+    
+    private final class Task: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result)-> Void)?
+        
+        init(completion: ((FeedImageDataLoader.Result) -> Void)? = nil) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletion()
+        }
+        
+        private func preventFurtherCompletion() {
+            completion = nil
+        }
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = Task(completion: completion)
+        store.retrieve(dataForURL: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                .mapError{ _ in Error.failed }
+                .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound)  })
+        }
+        return task
+    }
+}
--- CleanArchitectureTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ CleanArchitectureTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -8,57 +8,6 @@
 import XCTest
 import CleanArchitecture
 
-protocol FeedImageDataStore {
-    typealias Result = Swift.Result<Data?, Error>
-    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
-}
-
-final class LocalFeedImageDataLoader {
-    
-    private let store: FeedImageDataStore
-    
-    init(store: FeedImageDataStore) {
-        self.store = store
-    }
-    
-    public enum Error: Swift.Error {
-        case failed
-        case notFound
-    }
-    
-    private final class Task: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result)-> Void)?
-        
-        init(completion: ((FeedImageDataLoader.Result) -> Void)? = nil) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletion()
-        }
-        
-        private func preventFurtherCompletion() {
-            completion = nil
-        }
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = Task(completion: completion)
-        store.retrieve(dataForURL: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            task.complete(with: result
-                .mapError{ _ in Error.failed }
-                .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound)  })
-        }
-        return task
-    }
-}
-
 class LocalFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
