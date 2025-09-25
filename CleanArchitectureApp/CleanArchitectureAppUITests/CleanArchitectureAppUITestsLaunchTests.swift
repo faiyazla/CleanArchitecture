f12f2bbab4a860c59bd061debd752625fe1d39a0
faiyazla <fyizahmed@gmail.com>
2025-09-25 07:53:07 +0545

    Add empty CleanArchitectureApp project

---
+++ CleanArchitectureApp/CleanArchitectureAppUITests/CleanArchitectureAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CleanArchitectureAppUITestsLaunchTests.swift
+//  CleanArchitectureAppUITests
+//
+//  Created by Faiyaz Ahmed on 25/09/2025.
+//
+
+import XCTest
+
+final class CleanArchitectureAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
