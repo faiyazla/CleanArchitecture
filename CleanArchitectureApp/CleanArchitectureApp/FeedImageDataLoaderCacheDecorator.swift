0067a06a5ac9eff003239da7b0e05b27895f6e89
faiyazla <fyizahmed@gmail.com>
2025-09-26 11:04:12 +0545

    Move `FeedImageDataLoaderCacheDecorator` to production

---
+++ CleanArchitectureApp/CleanArchitectureApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+//
+//  FeedImageDataLoaderCacheDecorator.swift
+//  CleanArchitectureApp
+//
+//  Created by Faiyaz Ahmed on 26/09/2025.
+//
+
+import Foundation
+import CleanArchitecture
+
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+    
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            completion(result.map { data in
+                self?.cache.save(data, for: url) { _ in }
+                return data
+            })
+        }
+    }
+}
