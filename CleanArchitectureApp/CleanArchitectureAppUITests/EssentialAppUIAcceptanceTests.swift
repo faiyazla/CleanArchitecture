c5e40367d22161f0f6eccbc8ea36613bdae5b2b3
faiyazla <fyizahmed@gmail.com>
2025-09-29 10:23:06 +0545

    Add UI Test target for running high-level Acceptance Tests

---
+++ CleanArchitectureApp/CleanArchitectureAppUITests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  CleanArchitectureApp
+//
+//  Created by Faiyaz Ahmed on 29/09/2025.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+    
+}
