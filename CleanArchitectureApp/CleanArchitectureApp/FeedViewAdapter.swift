8e0650bce841798678561e91bcc78e8e422f4e90
faiyazla <fyizahmed@gmail.com>
2025-10-03 10:27:04 +0545

    Replace FeedImageDataLoader composition with Combine operators

--- CleanArchitectureApp/CleanArchitectureApp/FeedViewAdapter.swift
+++ CleanArchitectureApp/CleanArchitectureApp/FeedViewAdapter.swift
@@ -8,12 +8,13 @@
 import UIKit
 import CleanArchitecture
 import CleanArchitectureiOS
+import Combine
 
 final class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
-    private let imageLoader: FeedImageDataLoader
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
     
-    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+    init(controller: FeedViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.controller = controller
         self.imageLoader = imageLoader
     }
