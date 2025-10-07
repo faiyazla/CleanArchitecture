b635798a9bb456e7c0c3ef52d42330d020034a8a
faiyazla <fyizahmed@gmail.com>
2025-10-07 11:14:09 +0545

    Duplicate RemoteFeedLoader as RemoteLoader

---
+++ CleanArchitecture/Shared API/RemoteLoader.swift
@@ -0,0 +1,47 @@
+//
+//  RemoteLoader.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 07/10/2025.
+//
+
+import Foundation
+
+public final class RemoteLoader: FeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = FeedLoader.Result
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    public func load(completion: @escaping (Result) -> ()) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map (_ data: Data, _ response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
+
