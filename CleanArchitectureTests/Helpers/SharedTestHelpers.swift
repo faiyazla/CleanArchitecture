8db669452495d379a46fc51d4f09aefcb3f7ca65
faiyazla <fyizahmed@gmail.com>
2025-10-09 12:17:23 +0545

    Map ImageComments into ImageCommentsViewModels

--- CleanArchitectureTests/Helpers/SharedTestHelpers.swift
+++ CleanArchitectureTests/Helpers/SharedTestHelpers.swift
@@ -27,15 +27,19 @@ extension Date {
     private var feedCacheMaxAgeInDays: Int {
         return -7
     }
-    
-    private func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
 }
 
 extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
+    }
+    
+    func adding(minutes: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
 }
 func makeItemsJSON(_ items: [[String: Any]]) -> Data {
