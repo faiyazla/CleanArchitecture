df499834fcadde0a42068d7cd0782584f05e6f22
faiyazla <fyizahmed@gmail.com>
2025-10-13 11:34:51 +0545

    Implement Image Comments UI

---
+++ CleanArchitectureiOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCommentCellController.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 13/10/2025.
+//
+
+import UIKit
+import CleanArchitecture
+
+public final class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.dateLabel.text = model.date
+        cell.usernameLabel.text = model.username
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+}
---
+++ CleanArchitectureiOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  CleanArchitecture
+//
+//  Created by Faiyaz Ahmed on 13/10/2025.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+}
