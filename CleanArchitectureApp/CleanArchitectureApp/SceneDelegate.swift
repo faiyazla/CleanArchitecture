a899bf3a9ea44af01d189f3579f0d69fa84235ad
faiyazla <fyizahmed@gmail.com>
2025-11-01 17:02:44 +0545

    Cache page results

--- CleanArchitectureApp/CleanArchitectureApp/SceneDelegate.swift
+++ CleanArchitectureApp/CleanArchitectureApp/SceneDelegate.swift
@@ -123,7 +123,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private func makeRemoteLoadMoreLoader(items: [FeedImage], last: FeedImage?) -> (() -> AnyPublisher<Paginated<FeedImage>, Error>)? {
         last.map { lastItem in
             let url = FeedEndpoint.get(after: lastItem).url(baseURL: baseURL)
-            return { [httpClient] in
+            return { [httpClient, localFeedLoader] in
                 httpClient
                     .getPublisher(url: url)
                     .tryMap(FeedItemsMapper.map)
@@ -132,7 +132,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         return Paginated(items: allItems,
                                          loadMorePublisher: self.makeRemoteLoadMoreLoader(items: allItems,
                                                                                           last: newItems.last))
-                    }.eraseToAnyPublisher()
+                    }
+                    .caching(to: localFeedLoader)
             }
         }
     }
