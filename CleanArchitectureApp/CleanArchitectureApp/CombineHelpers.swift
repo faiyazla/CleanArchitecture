0075206bf15b930a251ae286e78708ffda72823e
faiyazla <fyizahmed@gmail.com>
2025-11-01 12:27:07 +0545

    Prevent load more action while loading more

--- CleanArchitectureApp/CleanArchitectureApp/CombineHelpers.swift
+++ CleanArchitectureApp/CleanArchitectureApp/CombineHelpers.swift
@@ -9,6 +9,19 @@ import Combine
 import Foundation
 import CleanArchitecture
 
+public extension Paginated {
+    
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+        guard let loadMore = loadMore else { return nil }
+        
+        return {
+            Deferred {
+                Future(loadMore)
+            }.eraseToAnyPublisher()
+        }
+    }
+}
+
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
     
